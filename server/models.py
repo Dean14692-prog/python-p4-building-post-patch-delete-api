38bdaac6a0437057186b4347a0d4a246ac9f67cc
Dean14692-prog <dennismuasyangui@gmail.com>
2025-06-18 17:37:28 +0300

    comit

--- server/models.py
+++ server/models.py
@@ -1,17 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Game(db.Model, SerializerMixin):
+class Game(db.Model):
     __tablename__ = 'games'
-
-    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -26,27 +18,19 @@ class Game(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model, SerializerMixin):
-    __tablename__ = 'reviews'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'genre': self.genre,
+            'platform': self.platform,
+            'price': self.price,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
 
-    serialize_rules = ('-game.reviews', '-user.reviews',)
-    
-    id = db.Column(db.Integer, primary_key=True)
-    score = db.Column(db.Integer)
-    comment = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
-
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     __tablename__ = 'users'
-
-    serialize_rules = ('-reviews.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -55,3 +39,43 @@ class User(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     reviews = db.relationship('Review', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+        }
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'score': self.score,
+            'comment': self.comment,
+            'game_id': self.game_id,
+            'user_id': self.user_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'user': self.user.to_dict(),
+            'game': self.game.to_dict(),
+        }
--- server/seed.py
+++ server/seed.py
@@ -1,110 +1,53 @@
 #!/usr/bin/env python3
 
-from random import randint, choice as rc
+from random import choice as rc, randint
 
 from faker import Faker
 
 from app import app
-from models import db, Game, Review, User
-
-genres = [
-    "Platformer",
-    "Shooter",
-    "Fighting",
-    "Stealth",
-    "Survival",
-    "Rhythm",
-    "Survival Horror",
-    "Metroidvania",
-    "Text-Based",
-    "Visual Novel",
-    "Tile-Matching",
-    "Puzzle",
-    "Action RPG",
-    "MMORPG",
-    "Tactical RPG",
-    "JRPG",
-    "Life Simulator",
-    "Vehicle Simulator",
-    "Tower Defense",
-    "Turn-Based Strategy",
-    "Racing",
-    "Sports",
-    "Party",
-    "Trivia",
-    "Sandbox"
-]
-
-platforms = [
-    "NES",
-    "SNES",
-    "Nintendo 64",
-    "GameCube",
-    "Wii",
-    "Wii U",
-    "Nintendo Switch",
-    "GameBoy",
-    "GameBoy Advance",
-    "Nintendo DS",
-    "Nintendo 3DS",
-    "XBox",
-    "XBox 360",
-    "XBox One",
-    "XBox Series X/S",
-    "PlayStation",
-    "PlayStation 2",
-    "PlayStation 3",
-    "PlayStation 4",
-    "PlayStation 5",
-    "PSP",
-    "PS Vita",
-    "Genesis",
-    "DreamCast",
-    "PC",
-]
+from models import db, User, Review, Game
 
 fake = Faker()
 
 with app.app_context():
-
+    
+    print("Deleting all records...")
     Review.query.delete()
     User.query.delete()
     Game.query.delete()
 
+    print("Creating users...")
     users = []
-    for i in range(100):
-        u = User(name=fake.name(),)
-        users.append(u)
-
+    for i in range(5):
+        user = User(name=fake.name())
+        users.append(user)
     db.session.add_all(users)
 
+    print("Creating games...")
     games = []
-    for i in range(100):
-        g = Game(
-            title=fake.sentence(),
+    genres = ['Action', 'Adventure', 'RPG', 'Strategy', 'Sports']
+    platforms = ['PC', 'PlayStation', 'Xbox', 'Nintendo Switch']
+    for i in range(10):
+        game = Game(
+            title=fake.unique.word(),
             genre=rc(genres),
             platform=rc(platforms),
-            price=randint(5, 60),
+            price=randint(10, 60)
         )
-        games.append(g)
-
+        games.append(game)
     db.session.add_all(games)
 
+    print("Creating reviews...")
     reviews = []
-    for u in users:
-        for i in range(randint(1, 10)):
-            r = Review(
-                score=randint(0, 10),
-                comment=fake.sentence(),
-                user=u,
-                game=rc(games))
-            reviews.append(r)
-
+    for i in range(25):
+        review = Review(
+            score=randint(1, 10),
+            comment=fake.sentence(),
+            game_id=randint(1, 10),
+            user_id=randint(1, 5)
+        )
+        reviews.append(review)
     db.session.add_all(reviews)
 
-    for g in games:
-        r = rc(reviews)
-        g.review = r
-        reviews.remove(r)
-
     db.session.commit()
+    print("Complete.")
